f9d5494cc421a7d883a8940a0b68a36937c8f737
The-Open-Tube-Project <open-tube@outlook.com>
2025-04-21 22:47:19 +0200

    Ajoutez des fichiers projet.

---
+++ HOTTUI/H.O.T.T.U.I/Texture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOTTUI.H.O.T.T.U.I
+{
+    public class Texture
+    {
+        public static void ReadTextureUNICOLOR(string filePath, List<string> FinalTexture)
+        {
+            var texture = new StreamReader(filePath);
+
+
+            for (int i = 0; i < texture.ReadToEnd().Length; i++)
+            {
+                string rest = texture.ReadLine();
+                FinalTexture.Add(rest);
+            }
+        }
+        public static void WhriteTextureUNICOLOR(List<string> FinalTexture, ConsoleColor color)
+        {
+            foreach (var texture in FinalTexture)
+            {
+                Tools.Color_Write(color, texture);
+            }
+        }
+    }
+}
---
+++ HOTTUI/H.O.T.T.U.I/Window.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOTTUI.H.O.T.T.U.I
+{
+    public class Window
+    {
+        public static void SetWindow(string? WindowName, ConsoleColor color)
+        {
+            int ConsoleSizeWidth = Console.WindowWidth;
+            int ConsoleSizeHeight = Console.WindowHeight;
+            char jsp = '-';
+            Console.Clear();
+            Console.Title = WindowName;
+            Tools.Horizontal_Line(ConsoleSizeWidth, color, jsp);
+            Tools.Color_Write(color, WindowName + "\n");
+            Tools.Horizontal_Line(ConsoleSizeWidth, color, jsp);
+        }
+    }
+}
